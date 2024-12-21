84f826945201066d09480942185ade732a7d6cc2
shafriki <shafriki.cse@gmail.com>
2024-12-21 18:48:45 +0600

    Bismillah starting ProtiDour b10a11

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment-11 B10A11 Programming Hero.
+
---
+++ src/Routes/Route.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Authentication/Login";
+import Register from "../Pages/Authentication/Register";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        errorElement: <h1>error page</h1>,
+        children: [
+            {
+                index: true, 
+                element: <Home></Home>,
+            },
+            {
+                path: '/home',
+                element: <Home></Home>,
+            },
+            {
+                path:'/login',
+                element:<Login></Login>,
+            },
+            {
+                path:'/register',
+                element:<Register></Register>,
+            },
+        ]
+    }
+])
+
+export default router;
