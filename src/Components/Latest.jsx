1f756c12b5ac9a96bf83be054ec3c2a87253fdab
shafriki <shafriki.cse@gmail.com>
2024-12-23 11:01:44 +0600

    upcoming marathons marquee design changed

--- src/Components/Latest.jsx
+++ src/Components/Latest.jsx
@@ -7,12 +7,30 @@ import { MdLabelImportant } from "react-icons/md";
 const Latest = () => {
     return (
         <div className='flex mx-2 mb-5 items-center  gap-2 bg-base-200 p-2 text-xs md:text-base bg-gradient-to-r from-[#1B1B1D] via-[#272730] to-[#6E2B4E] text-white'>
-            <p className='bg-[#228B22] px-4 py-2 text-white'>Latest</p>
+            <p className='bg-[#228B22] px-4 py-2 text-white whitespace-nowrap'>Upcoming Marathons</p>
 
             <Marquee autoFill='ture' pauseOnHover='ture'>
                 <Link className='ml-10 flex items-center gap-1'><MdLabelImportant  className='text-[#228B22] text-xl'/>
-                Match Highlights: Germany vs Spain — as it happened   !</Link>
-                <Link className='ml-10 flex items-center gap-1'><MdLabelImportant  className='text-[#228B22] text-xl'/>Match Highlights: Germany vs Spain as...</Link>
+                Dhaka City Marathon 2024</Link>
+
+                <Link className='ml-10 flex items-center gap-1'><MdLabelImportant  className='text-[#228B22] text-xl'/>Chattogram Coastal Run
+                </Link>
+
+                <Link className='ml-10 flex items-center gap-1'><MdLabelImportant  className='text-[#228B22] text-xl'/>Sylhet Tea Garden Trail
+                </Link>
+
+                <Link className='ml-10 flex items-center gap-1'><MdLabelImportant  className='text-[#228B22] text-xl'/>Rajshahi Heritage Marathon
+                </Link>
+
+                <Link className='ml-10 flex items-center gap-1'><MdLabelImportant  className='text-[#228B22] text-xl'/>Cox’s Bazar Beach Run
+                </Link>
+
+                <Link className='ml-10 flex items-center gap-1'><MdLabelImportant  className='text-[#228B22] text-xl'/>Khulna Eco Run
+                </Link>
+
+                <Link className='ml-10 flex items-center gap-1'><MdLabelImportant  className='text-[#228B22] text-xl'/>Barisal Riverfront Run
+                </Link>
+
             </Marquee>
         </div>
     );
